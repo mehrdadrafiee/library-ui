d6bd8f9451b98f6a5ba20a1ee459c68794101a34
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2018-08-08 23:43:32 -0700

    adds author details

--- app/routes/author/detail.js
+++ app/routes/author/detail.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model({ id }) {
+    return {
+      first: 'J.K',
+      last: 'Rowlin',
+      id: `author-${id}`
+    };
+  }
 });
